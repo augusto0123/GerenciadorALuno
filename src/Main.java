6be5f34b9a4098a8dcebbba98eb8f37fa117f0f5
Augusto <senescal.oliveira@gmail.com>
2025-03-26 10:15:48 -0300

    feat: adicionado funcao para adicionar alunos na lista

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
+import classe.Classe;
+import model.Aluno;
 
 public class Main {
     public static void main(String[] args) {
 
+        Classe classe = new Classe();
+        Aluno aluno1 = new Aluno("Augusto", 75, "Artes");
+        Aluno aluno2 = new Aluno("Lívia", 100, "Artes");
+        Aluno aluno3 = new Aluno("Maria", 85, "Matemática");
+        Aluno aluno4 = new Aluno("José", 90, "Português");
 
+        classe.adicionarAluno(aluno1);
+        classe.adicionarAluno(aluno2);
+        classe.adicionarAluno(aluno3);
+        classe.adicionarAluno(aluno4);
     }
 }
--- src/classe/Classe.java
+++ src/classe/Classe.java
@@ -1,5 +1,16 @@
 package classe;
 
+import model.Aluno;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Classe {
+
+    private List<Aluno> alunos = new ArrayList<>();
+
+    public void adicionarAluno(Aluno aluno){
+        alunos.add(aluno);
+    }
 
 }
