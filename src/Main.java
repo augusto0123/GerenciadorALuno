45a7e4e7df4c3dd3aa751b0eda12a4d2573e492f
Augusto <senescal.oliveira@gmail.com>
2025-03-26 10:24:01 -0300

    feat: adicionado funcao para ordenar notas em ordem crescente

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,34 @@
 import classe.Classe;
 import model.Aluno;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
         Classe classe = new Classe();
+
         Aluno aluno1 = new Aluno("Augusto", 75, "Artes");
         Aluno aluno2 = new Aluno("Lívia", 100, "Artes");
         Aluno aluno3 = new Aluno("Maria", 85, "Matemática");
         Aluno aluno4 = new Aluno("José", 90, "Português");
 
+        // adicionando alunos na lista
         classe.adicionarAluno(aluno1);
         classe.adicionarAluno(aluno2);
         classe.adicionarAluno(aluno3);
         classe.adicionarAluno(aluno4);
+
+        // buscando alunos por nome
+        System.out.println("Forneça o nome do aluno: ");
+        String nomeFornecido = scanner.nextLine();
+
+        classe.buscarAlunoPorNome(nomeFornecido)
+                .forEach(aluno -> System.out.println("Nota: " + aluno.getNota() + " | Disciplina: " + aluno.getDisciplina()));
+
+        // Listando alunos com base na nota em ordem crescente
+        classe.listarAlunosPorNotaCrescente()
+                .forEach(aluno -> System.out.println("Nome: " + aluno.getNome() + " | Nota: " + aluno.getNota()));
     }
 }
--- src/classe/Classe.java
+++ src/classe/Classe.java
@@ -3,14 +3,32 @@ package classe;
 import model.Aluno;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Classe {
 
+    // 1° Criar lista
     private List<Aluno> alunos = new ArrayList<>();
 
+    // 2° Funcao para adicionar aluno
     public void adicionarAluno(Aluno aluno){
         alunos.add(aluno);
+    }
+
+    // 3° Funcao para buscar aluno por atributo
+    public List<Aluno> buscarAlunoPorNome(String nome){
+        return alunos.stream()
+                .filter(aluno -> aluno.getNome().equalsIgnoreCase(nome))
+                .collect(Collectors.toList());
+    }
+
+    // 4 Funcao para ordenar alunos por nota
+    public List<Aluno> listarAlunosPorNotaCrescente(){
+        return alunos.stream()
+                .sorted(Comparator.comparingInt(Aluno::getNota))
+                .collect(Collectors.toList());
     }
 
 }
